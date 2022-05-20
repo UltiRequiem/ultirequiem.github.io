09eee976062510af9530c94d44f3923d593d7d64
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-20 14:50:23 -0500

    :recycle: Update

--- mod.ts
+++ mod.ts
@@ -1,2 +1,10 @@
 // Just to setup the repo to TypeScript 😆
+
+import { fibonacciSequence } from "https://deno.land/x/fibonacci/mod.ts";
+import { randomNumber } from "https://deno.land/x/random_number/mod.ts";
+
 console.log("I love Deno 🦕");
+
+for (const fibonacci of fibonacciSequence(10)) {
+  console.log(fibonacci * randomNumber());
+}
